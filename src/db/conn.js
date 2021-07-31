23062368297e462a10394e526027628c84423cca
Aman <mandalamankumar.786@gmail.com>
2021-07-31 12:31:12 +0530

    my host

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/mydata",{
+mongoose.connect('mongodb+srv://aman:Mamata@4457@cluster0.th54q.mongodb.net/myfirstwebsite?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    useFindAndModify:false
 }).then(()=>{
     console.log("connection successful")
 }).catch((error)=>{
-    console.log(error);
+    console.log(`no connection`);
 })

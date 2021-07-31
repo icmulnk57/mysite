4e5fd5f2ccbaf9bc4b20f5d69cf715b242fa2560
Aman <mandalamankumar.786@gmail.com>
2021-07-31 08:16:03 +0530

    heroku host

---
+++ src/models/userdata.js
@@ -0,0 +1,40 @@
+// schema
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    email: {
+        type:String,
+        required:true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error("invalid email id")
+            }
+        }
+    },
+    phone: {
+        type: Number,
+        required: true,
+        min: 10
+    },
+    message: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+
+// collect data
+const User = mongoose.model("User",userSchema);
+module.exports =User;
